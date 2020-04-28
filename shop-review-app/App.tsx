016b90baa8edd89c5dfedc301740a02bed35cb2b
Takahiko Wada <wada.takahiko@gmail.com>
2020-04-28 12:55:35 +0900

    Added react-navigation

--- shop-review-app/App.tsx
+++ shop-review-app/App.tsx
@@ -1,43 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, FlatList, View, SafeAreaView } from "react-native";
-/* lib */
-import { getShops } from "./src/lib/firebase";
-/* components */
-import { ShopReviewItem } from "./src/components/ShopReviewItem";
-/* types */
-import { Shop } from "./src/types/shop";
+import React from "react";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  const [shops, setShops] = useState<Shop[]>([]);
-
-  useEffect(() => {
-    getFirebaseItems();
-  }, []);
-
-  const getFirebaseItems = async () => {
-    const shops = await getShops();
-    setShops(shops);
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={shops}
-        renderItem={({ item }: { item: Shop }) => (
-          <ShopReviewItem shop={item} />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-        numColumns={2}
-      />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ shop-review-app/src/screens/HomeScreen.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, FlatList, View, SafeAreaView } from "react-native";
+/* lib */
+import { getShops } from "../lib/firebase";
+/* components */
+import { ShopReviewItem } from "../components/ShopReviewItem";
+/* types */
+import { Shop } from "../types/shop";
+
+export const HomeScreen = () => {
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops();
+    setShops(shops);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={shops}
+        renderItem={({ item }: { item: Shop }) => (
+          <ShopReviewItem shop={item} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
