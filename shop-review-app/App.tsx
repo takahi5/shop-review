18b25d69f1d79daaa89ffac348511d0cb65168bb
Takahiko Wada <wada.takahiko@gmail.com>
2020-05-02 03:05:02 +0900

    Revert "Added react-navigation"
    This reverts commit 8748dc5ffa363ce7748cd07398668d036debce5c.

--- shop-review-app/App.tsx
+++ shop-review-app/App.tsx
@@ -1,6 +1,43 @@
-import React from "react";
-import AppNavigator from "./src/navigation/AppNavigator";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, FlatList, View, SafeAreaView } from "react-native";
+/* lib */
+import { getShops } from "./src/lib/firebase";
+/* components */
+import { ShopReviewItem } from "./src/components/ShopReviewItem";
+/* types */
+import { Shop } from "./src/types/shop";
 
 export default function App() {
-  return <AppNavigator />;
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops();
+    setShops(shops);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={shops}
+        renderItem={({ item }: { item: Shop }) => (
+          <ShopReviewItem shop={item} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
