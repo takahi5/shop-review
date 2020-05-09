bc4a66d02ec54e96817cbb778cf19cf9c3480ac4
Takahiko Wada <wada.takahiko@gmail.com>
2020-05-09 13:38:41 +0900

    userドキュメントの更新

--- shop-review-app/src/screens/UserScreen.tsx
+++ shop-review-app/src/screens/UserScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { updateUser } from "../lib/firebase";
+import firebase from "firebase";
 /* components */
 import { Form } from "../components/Form";
 import { Button } from "../components/Button";
@@ -16,13 +18,19 @@ type Props = {
 };
 
 export const UserScreen: React.FC<Props> = ({ navigation, route }: Props) => {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const [name, setName] = useState<string>(user.name);
+
+  const onSubmit = async () => {
+    const updatedAt = firebase.firestore.Timestamp.now();
+    await updateUser(user.id, { name, updatedAt });
+    setUser({ ...user, name, updatedAt });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <Form value={name} onChangeText={(text) => setName(text)} label="名前" />
-      <Button onPress={() => {}} text="保存する" />
+      <Button onPress={onSubmit} text="保存する" />
     </SafeAreaView>
   );
 };
