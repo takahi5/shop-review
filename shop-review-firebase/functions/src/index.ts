c7d3d379ed1fc774f7b20cb60966f0e715a39304
Takahiko Wada <wada.takahiko@gmail.com>
2020-05-10 14:30:38 +0900

    user更新時にreview.userを更新するfunctions

--- shop-review-firebase/functions/src/index.ts
+++ shop-review-firebase/functions/src/index.ts
@@ -3,8 +3,11 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
+type User = {
+  name: string;
+};
+
 type Review = {
-  text: string;
   score: number;
 };
 
@@ -18,6 +21,30 @@ type Shop = {
   score4: number;
   score5: number;
 };
+
+exports.onUpdateUser = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{userId}")
+  .onUpdate(async (change, context) => {
+    const { userId } = context.params;
+    const newUser = change.after.data() as User;
+
+    const db = admin.firestore();
+    try {
+      const snapshot = await db
+        .collectionGroup("reviews")
+        .where("user.id", "==", userId)
+        .get();
+      const batch = db.batch();
+      snapshot.docs.forEach((reviewDoc) => {
+        const user = { ...reviewDoc.data().user, name: newUser.name };
+        batch.update(reviewDoc.ref, { user });
+      });
+      await batch.commit();
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 exports.onWriteReview = functions
   .region("asia-northeast1")
