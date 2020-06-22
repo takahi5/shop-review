9c0bebeb3032afb9a87f70509c869f7f0c2be3ae
Takahiko Wada <wada.takahiko@gmail.com>
2020-06-22 09:50:32 +0900

    functionでreviewのuser.name更新

--- shop-review-firebase/functions/src/index.ts
+++ shop-review-firebase/functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from "firebase-functions";
+import { User } from "./types/user";
+import admin = require("firebase-admin");
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+exports.onUpdateUser = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{userId}")
+  .onUpdate(async (change, context) => {
+    const { userId } = context.params;
+    const newUser = change.after.data() as User;
+
+    const db = admin.firestore();
+    try {
+      const snapshot = await db
+        .collectionGroup("reviews")
+        .where("user.id", "==", userId)
+        .get();
+
+      const batch = db.batch();
+      snapshot.docs.forEach((reviewDoc) => {
+        const user = { ...reviewDoc.data().user, name: newUser.name };
+        batch.update(reviewDoc.ref, { user });
+      });
+      await batch.commit();
+    } catch (err) {
+      console.log(err);
+    }
+  });
