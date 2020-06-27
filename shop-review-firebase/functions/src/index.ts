1dcec018bd5db429d4c42aedc11442799479af87
Takahiko Wada <wada.takahiko@gmail.com>
2020-06-28 05:01:41 +0900

    平均スコアの計算

--- shop-review-firebase/functions/src/index.ts
+++ shop-review-firebase/functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import { User } from "./types/user";
 import admin = require("firebase-admin");
+import { Review } from "./types/review";
+import { Shop } from "./types/shop";
 
 admin.initializeApp();
 
@@ -24,6 +26,75 @@ exports.onUpdateUser = functions
         batch.update(reviewDoc.ref, { user });
       });
       await batch.commit();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+exports.onWriteReview = functions
+  .region("asia-northeast1")
+  .firestore.document("shops/{shopId}/reviews/{reviewId}")
+  .onWrite(async (change, context) => {
+    const { shopId } = context.params;
+    const review = change.after.data() as Review;
+    const db = admin.firestore();
+    try {
+      const shopRef = db.collection("shops").doc(shopId);
+      const shopDoc = await shopRef.get();
+      const shop = shopDoc.data() as Shop;
+
+      // 平均scoreの計算
+      let { score1 = 0, score2 = 0, score3 = 0, score4 = 0, score5 = 0 } = shop;
+      if (review.score === 1) {
+        score1 += 1;
+      } else if (review.score === 2) {
+        score2 += 1;
+      } else if (review.score === 3) {
+        score3 += 1;
+      } else if (review.score === 4) {
+        score4 += 1;
+      } else if (review.score === 5) {
+        score5 += 1;
+      }
+      let aveScore =
+        (score1 + score2 * 2 + score3 * 3 + score4 * 4 + score5 * 5) /
+        (score1 + score2 + score3 + score4 + score5);
+      aveScore = Math.round(aveScore * 100) / 100;
+
+      // shopの更新
+      let params = {};
+      if (review.score === 1) {
+        params = {
+          score1: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      } else if (review.score === 2) {
+        params = {
+          score2: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      } else if (review.score === 3) {
+        params = {
+          score3: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      } else if (review.score === 3) {
+        params = {
+          score3: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      } else if (review.score === 4) {
+        params = {
+          score4: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      } else if (review.score === 5) {
+        params = {
+          score5: admin.firestore.FieldValue.increment(1),
+          score: aveScore,
+        };
+      }
+      await shopRef.update(params);
     } catch (err) {
       console.log(err);
     }
