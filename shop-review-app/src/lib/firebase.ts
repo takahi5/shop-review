3dd10b50b0e7d502241ad42548e61ecf995f8beb
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-15 05:58:31 +0900

    ショップのソート

--- shop-review-app/src/lib/firebase.ts
+++ shop-review-app/src/lib/firebase.ts
@@ -9,7 +9,11 @@ if (!firebase.apps.length) {
 }
 
 export const getShops = async () => {
-  const snapshot = await firebase.firestore().collection("shops").get();
+  const snapshot = await firebase
+    .firestore()
+    .collection("shops")
+    .orderBy("score", "desc")
+    .get();
   const shops = snapshot.docs.map((doc) => doc.data() as Shop);
   return shops;
 };
