079fd55fc602042cda151a1e8b95cf68537f50c8
Takahiko Wada <wada.takahiko@gmail.com>
2020-09-15 17:31:05 +0900

    レビュー一覧表示

--- shop-review-app/src/screens/ShopScreen.tsx
+++ shop-review-app/src/screens/ShopScreen.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { getReviews } from "../lib/firebase";
 /* components */
 import { ShopDetail } from "../components/ShopDetail";
 import { FloatingActionButton } from "../components/FloatingActionButton";
+import { ReviewItem } from "../components/ReviewItem";
 /* types */
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../types/navigation";
+import { Review } from "../types/review";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList, "Shop">;
@@ -15,14 +18,28 @@ type Props = {
 
 export const ShopScreen: React.FC<Props> = ({ navigation, route }: Props) => {
   const { shop } = route.params;
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
-    navigation.setOptions({ title: shop.name });
+    navigation.setOptions({
+      title: shop.name,
+    });
+
+    const fetchReviews = async () => {
+      const reviews = await getReviews(shop.id);
+      setReviews(reviews);
+    };
+    fetchReviews();
   }, [shop]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <ShopDetail shop={shop} />
+      <FlatList
+        ListHeaderComponent={<ShopDetail shop={shop} />}
+        data={reviews}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+        keyExtractor={(item) => item.id}
+      />
       <FloatingActionButton
         iconName="plus"
         onPress={() => navigation.navigate("CreateReview", { shop })}
